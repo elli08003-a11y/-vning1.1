8700c927ce8f3cb3ac867dd672cb09f8b6dd93eb
elli08003 <elli08003@E-PW04SM2Y>
2025-09-03 12:58:58 +0200

    Add project files.

---
+++ Övning1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Övning1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // WrigteLine låte de två olika texterna vara på sin egan rad.
+            Console.WriteLine("Du är nestan klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som\" och");
+        }
+    }
+}
